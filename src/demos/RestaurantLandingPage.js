0b7d2b3f5fac951a589e78c8937726128b6f54e4
Jack Maddox <jackdmaddox@gmail.com>
2020-06-26 18:52:39 +0100

    lazy load menu

--- src/demos/RestaurantLandingPage.js
+++ src/demos/RestaurantLandingPage.js
@@ -1,18 +1,19 @@
-import React from "react";
-import tw from "twin.macro";
-import { css } from "styled-components/macro"; //eslint-disable-line
-import Hero from "components/hero/TwoColumnWithVideo.js";
-import Features from "components/features/ThreeColSimple.js";
-import MainFeature from "components/features/TwoColWithButton.js";
-import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
-import TabGrid from "components/cards/TabCardGrid.js";
-import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
-import DownloadApp from "components/cta/DownloadApp.js";
-import Footer from "components/footers/FiveColumnWithInputForm.js";
+import React from 'react';
+import tw from 'twin.macro';
+import { css } from 'styled-components/macro'; //eslint-disable-line
+import Hero from 'components/hero/TwoColumnWithVideo.js';
+import Features from 'components/features/ThreeColSimple.js';
+import MainFeature from 'components/features/TwoColWithButton.js';
+import MainFeature2 from 'components/features/TwoColSingleFeatureWithStats2.js';
+import TabGrid from 'components/cards/TabCardGrid.js';
+import Testimonial from 'components/testimonials/ThreeColumnWithProfileImage.js';
+import DownloadApp from 'components/cta/DownloadApp.js';
+import Footer from 'components/footers/FiveColumnWithInputForm.js';
 
-import chefIconImageSrc from "images/chef-icon.svg";
-import celebrationIconImageSrc from "images/celebration-icon.svg";
-import shopIconImageSrc from "images/shop-icon.svg";
+import chefIconImageSrc from 'images/chef-icon.svg';
+import celebrationIconImageSrc from 'images/celebration-icon.svg';
+import shopIconImageSrc from 'images/shop-icon.svg';
+import LazyLoad from 'react-lazyload';
 
 export default () => {
   const Subheading = tw.span`tracking-wider text-sm font-medium`;
@@ -20,9 +21,15 @@ export default () => {
   const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
   const Description = tw.span`inline-block mt-8`;
   const imageCss = tw`rounded-4xl`;
-  return (<>
+  return (
+    <>
       <Hero
-        heading={<>Delicious & Affordable <HighlightedText>Meals Near You.</HighlightedText></>}
+        heading={
+          <>
+            Delicious & Affordable{' '}
+            <HighlightedText>Meals Near You.</HighlightedText>
+          </>
+        }
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         imageSrc="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
         imageCss={imageCss}
@@ -40,31 +47,34 @@ export default () => {
         }
         description={
           <Description>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <br />
             <br />
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
           </Description>
         }
         buttonRounded={false}
         textOnLeft={false}
         primaryButtonText="Latest Offers"
         imageSrc={
-          "https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+          'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80'
         }
         imageCss={imageCss}
         imageDecoratorBlob={true}
         imageDecoratorBlobCss={tw`left-1/2 -translate-x-1/2 md:w-32 md:h-32 opacity-25`}
       />
       {/* TabGrid Component also accepts a tabs prop to customize the tabs and its content directly. Please open the TabGrid component file to see the structure of the tabs props.*/}
-      <TabGrid
-        heading={
-          <>
-            Checkout our <HighlightedText>menu.</HighlightedText>
-          </>
-        }
-      />
+      <LazyLoad>
+        <TabGrid
+          heading={
+            <>
+              Checkout our <HighlightedText>menu.</HighlightedText>
+            </>
+          }
+        />
+      </LazyLoad>
       <Features
         heading={
           <>
@@ -74,43 +84,46 @@ export default () => {
         cards={[
           {
             imageSrc: shopIconImageSrc,
-            title: "230+ Locations",
-            description: "Lorem ipsum donor amet siti ceali placeholder text",
-            url: "https://google.com"
+            title: '230+ Locations',
+            description: 'Lorem ipsum donor amet siti ceali placeholder text',
+            url: 'https://google.com',
           },
           {
             imageSrc: chefIconImageSrc,
-            title: "Professional Chefs",
-            description: "Lorem ipsum donor amet siti ceali placeholder text",
-            url: "https://timerse.com"
+            title: 'Professional Chefs',
+            description: 'Lorem ipsum donor amet siti ceali placeholder text',
+            url: 'https://timerse.com',
           },
           {
             imageSrc: celebrationIconImageSrc,
-            title: "Birthday Catering",
-            description: "Lorem ipsum donor amet siti ceali placeholder text",
-            url: "https://reddit.com"
-          }
+            title: 'Birthday Catering',
+            description: 'Lorem ipsum donor amet siti ceali placeholder text',
+            url: 'https://reddit.com',
+          },
         ]}
-
         imageContainerCss={tw`p-2!`}
         imageCss={tw`w-20! h-20!`}
       />
       <MainFeature2
         subheading={<Subheading>A Reputed Brand</Subheading>}
-        heading={<>Why <HighlightedText>Choose Us ?</HighlightedText></>}
+        heading={
+          <>
+            Why <HighlightedText>Choose Us ?</HighlightedText>
+          </>
+        }
         statistics={[
           {
-            key: "Orders",
-            value: "94000+",
+            key: 'Orders',
+            value: '94000+',
           },
           {
-            key: "Customers",
-            value: "11000+"
+            key: 'Customers',
+            value: '11000+',
           },
           {
-            key: "Chefs",
-            value: "1500+"
-          }
+            key: 'Chefs',
+            value: '1500+',
+          },
         ]}
         primaryButtonText="Order Now"
         primaryButtonUrl="https://order.now.com"
@@ -124,12 +137,21 @@ export default () => {
       />
       <Testimonial
         subheading=""
-        heading={<>Customers <HighlightedText>Love Us.</HighlightedText></>}
+        heading={
+          <>
+            Customers <HighlightedText>Love Us.</HighlightedText>
+          </>
+        }
       />
       <DownloadApp
-        text={<>People around you are ordering delicious meals using the <HighlightedTextInverse>Treact App.</HighlightedTextInverse></>}
+        text={
+          <>
+            People around you are ordering delicious meals using the{' '}
+            <HighlightedTextInverse>Treact App.</HighlightedTextInverse>
+          </>
+        }
       />
       <Footer />
-      </>
+    </>
   );
-}
+};
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,25 +1,25 @@
-import React from "react";
-import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-import tw from "twin.macro";
-import styled from "styled-components"; //eslint-disable-line
-import { css } from "styled-components/macro"; //eslint-disable-line
-import Header from "components/headers/light.js";
-import Footer from "components/footers/FiveColumnWithInputForm.js";
-import MainFeature1 from "components/features/TwoColWithButton.js";
+import React from 'react';
+import AnimationRevealPage from 'helpers/AnimationRevealPage.js';
+import tw from 'twin.macro';
+import styled from 'styled-components'; //eslint-disable-line
+import { css } from 'styled-components/macro'; //eslint-disable-line
+import Header from 'components/headers/light.js';
+import Footer from 'components/footers/FiveColumnWithInputForm.js';
+import MainFeature1 from 'components/features/TwoColWithButton.js';
 // import MainFeature2 from "components/features/TwoColSingleFeatureWithStats.js";
 // import MainFeature3 from "components/features/TwoColSingleFeatureWithStats2.js";
-import Features from "components/features/ThreeColSimple.js";
+import Features from 'components/features/ThreeColSimple.js';
 // import Features from "components/features/ThreeColWithSideImage.js";
-import TeamCardGrid from "components/cards/ProfileThreeColGrid.js";
+import TeamCardGrid from 'components/cards/ProfileThreeColGrid.js';
 
-import SupportIconImage from "images/support-icon.svg";
-import ShieldIconImage from "images/shield-icon.svg";
-import CustomerLoveIconImage from "images/simple-icon.svg";
+import SupportIconImage from 'images/support-icon.svg';
+import ShieldIconImage from 'images/shield-icon.svg';
+import CustomerLoveIconImage from 'images/simple-icon.svg';
 
 const Subheading = tw.span`uppercase tracking-wider text-sm`;
 export default () => {
   return (
-    <>
+    <AnimationRevealPage>
       <Header />
       <MainFeature1
         subheading={<Subheading>About Treact</Subheading>}
@@ -43,26 +43,27 @@ export default () => {
         cards={[
           {
             imageSrc: SupportIconImage,
-            title: "24/7 Support",
-            description: "Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport"
+            title: '24/7 Support',
+            description:
+              'Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport',
           },
           {
             imageSrc: ShieldIconImage,
-            title: "Strong Teams",
-            description: "Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport"
+            title: 'Strong Teams',
+            description:
+              'Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport',
           },
           {
             imageSrc: CustomerLoveIconImage,
-            title: "Customer Satisfaction",
-            description: "Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport"
+            title: 'Customer Satisfaction',
+            description:
+              'Lorem ipsum donor amet siti ceali placeholder text alipiscing elit sed do eiusmod temport',
           },
         ]}
         linkText=""
       />
-      <TeamCardGrid 
-        subheading={<Subheading>Our Team</Subheading>}
-      />
+      <TeamCardGrid subheading={<Subheading>Our Team</Subheading>} />
       <Footer />
-    </>
+    </AnimationRevealPage>
   );
 };
